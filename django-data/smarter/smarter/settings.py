f65410cee907d4cde335c572cbfd7808a96b85e5
Paolo Cozzi <bunop@libero.it>
2023-02-15 14:55:15 +0100

    :sparkles: manage site using bootstrap

--- django-data/smarter/smarter/settings.py
+++ django-data/smarter/smarter/settings.py
@@ -32,13 +32,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'repository.apps.RepositoryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
+    'repository.apps.RepositoryConfig',
 ]
 
 MIDDLEWARE = [
