da0b6e497c1fd29c2c12f2100666267635d6ab3d
Paolo Cozzi <bunop@libero.it>
2023-02-16 17:24:56 +0100

    :sparkles: deal with authentication

--- django-data/smarter/smarter/urls.py
+++ django-data/smarter/smarter/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
 
 from repository.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('login', LoginView.as_view(), name="login"),
+    path('logout', LogoutView.as_view(), name="logout"),
     path('', IndexView.as_view(), name="index")
 ]
