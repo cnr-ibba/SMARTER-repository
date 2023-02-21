b4cd1e40320db8877959a3f3f1fee0e12587a599
Paolo Cozzi <bunop@libero.it>
2023-02-21 12:24:30 +0100

    :card_file_box: define dataset model with admin stuff

--- django-data/smarter/repository/admin.py
+++ django-data/smarter/repository/admin.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 21 12:08:41 2023
+
+@author: Paolo Cozzi <paolo.cozzi@ibba.cnr.it>
+"""
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Dataset
+
+
+class DatasetAdmin(admin.ModelAdmin):
+    list_display = (
+        'data_type',
+        'wp',
+        'task',
+        'contact',
+        'submitted'
+    )
+
+
+admin.site.register(Dataset, DatasetAdmin)
--- django-data/smarter/repository/models.py
+++ django-data/smarter/repository/models.py
@@ -1,3 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 21 11:53:39 2023
+
+@author: Paolo Cozzi <paolo.cozzi@ibba.cnr.it>
+"""
+
+from enum import Enum
+
 from django.db import models
 
-# Create your models here.
+
+class SPECIES(Enum):
+    sheep = (0, "Sheep")
+    goat = (1, "Goat")
+
+
+class Dataset(models.Model):
+    wp = models.IntegerField(help_text="SMARTER Work Package number (ex. 4")
+    task = models.CharField(
+        max_length=50,
+        help_text="SMARTER taks (ex 2.1)"
+    )
+    species = models.SmallIntegerField(
+        choices=[x.value for x in SPECIES],
+        help_text="Species (Goat or Sheep)"
+    )
+    data_type = models.CharField(
+        max_length=255,
+        help_text="Data description"
+    )
+    owner = models.CharField(
+        max_length=50,
+        help_text="Who owns data (ex. INRAE)"
+    )
+    embargo = models.BooleanField(null=True)
+    time_series = models.BooleanField(null=True)
+    individual_data = models.BooleanField(null=True)
+    # HINT: should this be an email?
+    contact = models.CharField(max_length=50)
+    submitted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.data_type} (WP{self.wp})"
